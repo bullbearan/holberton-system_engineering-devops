6b65e4e29957642bb9e651206c6fd86040f51727
bullbearan <4403@holbertonstudents.com>
2022-08-14 17:58:33 +0000

    fix 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,9 @@
 # This is a line of text
+file_line { ' use_school_key':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
 file_line { 'set_password_authentication_to_no':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
