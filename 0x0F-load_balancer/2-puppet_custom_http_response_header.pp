3ceaf71006adf19d33a890942f0cae60bf6f010f
bullbearan <4403@holbertonstudents.com>
2022-08-22 22:16:01 +0100

    test

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# This is a line of text
+exec { 'update':
+  command => '/usr/bin/apt update',
+}
+
+package { 'nginx':
+    ensure  => present,
+}
+
+file_line {'header':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen 80 default_server;',
+    line    => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+    ensure  => running,
+}
