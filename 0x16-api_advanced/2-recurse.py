f04dd6dc753b7856ac1bdc5c192b14cbc6489b5f
bullbearan <4403@holbertonstudents.com>
2022-09-27 13:35:55 +0100

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 "This is a line of text"
-
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
     " This is a line of text"
-    sub = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
-                       headers={'User-Agent': "helo"}, params={'after': after})
+    url = "https://www.reddit.com"
+    sub = requests.get(f"{url}/r/{subreddit}/hot.json?after={after}",
+                       headers={'User-Agent': "hello"})
     if sub.status_code == 200:
         for child in sub.json().get("data").get("children"):
             hot_list.append(child.get("data").get("title"))
