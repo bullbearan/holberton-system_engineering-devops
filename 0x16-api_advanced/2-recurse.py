d23ff6a9181af2b1af957120b0d12f0bb222364e
bullbearan <4403@holbertonstudents.com>
2022-09-27 13:43:09 +0100

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"This is a line of text"
+" This is a line of text "
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    " This is a line of text"
-    url = "https://www.reddit.com"
-    sub = requests.get(f"{url}/r/{subreddit}/hot.json?after={after}",
-                       headers={'User-Agent': "hello"})
-    if sub.status_code == 200:
-        for child in sub.json().get("data").get("children"):
-            hot_list.append(child.get("data").get("title"))
-        after = sub.json().get("data").get("after")
-        if after is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
-    else:
+    " This is a line of text "
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'hello'}
+    params = {'after': after}
+    sub = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
+    if sub.status_code != 200:
         return None
+    for child in sub.json().get('data').get('children'):
+        hot_list.append(child.get('data').get('title'))
+    after = sub.json().get('data').get('after')
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
